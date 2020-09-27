ba9a65e1020db850c0bce4af975ab9aff3524f7e
Tobias <tobi5457@edu.easj.dk>
2020-09-27 17:05:40 +0200

    Midtvejs i header

--- js/scrollSnappy.js
+++ js/scrollSnappy.js
@@ -25,6 +25,8 @@ let counterScroller = 0;
 function scroll() {
     setTimeout(function () {
         window.scrollTo(0, heightOfWindow * counterScroller);
+
+        cycleNav();
     }, 300);
 
     bodyContainers = document.querySelectorAll(".heightWindow").forEach((elm) => {
@@ -69,3 +71,73 @@ function onScroll(){
 
 window.addEventListener("wheel", onScroll);
 
+
+
+
+//Header menu
+
+let header = document.querySelector("header");
+let nav = document.querySelector(".navContainer > nav");
+let navHeight = nav.clientHeight;
+let navElmCycle = document.querySelector(".navCycle");
+
+let pageUp = document.querySelector(".fa-chevron-up");
+let pageDown = document.querySelector(".fa-chevron-down");
+
+function cycleNav() {
+    if (counterScroller === 0){
+        header.classList.add("hideHeader");
+        if (widthOfWindow < 576){
+            navElmCycle.style.marginTop = (1) * - navHeight + "px";
+        }
+    } else {
+        header.classList.remove("hideHeader");
+        if (widthOfWindow < 576){
+            navElmCycle.style.marginTop = (counterScroller) * - navHeight + "px";
+        }
+    }
+}
+
+pageUp.addEventListener("click",() => {
+    if (counterScroller > 0 && counterScroller < amountOfBodyContainers){
+        counterScroller --;
+        console.log("up", counterScroller);
+        scroll();
+
+        //Pausing scrolling
+        pauseScrollListener();
+    }
+});
+
+pageDown.addEventListener("click",() => {
+    if (counterScroller >= 0 && counterScroller < amountOfBodyContainers - 1){
+        counterScroller ++;
+        console.log("down", counterScroller);
+        scroll();
+
+        //Pausing scrolling
+        pauseScrollListener();
+    }
+});
+
+document.querySelector("header > .topLogo").addEventListener("click",() => {
+    if (counterScroller > 0){
+        counterScroller = 0;
+        console.log("to top", counterScroller);
+        scroll();
+
+        //Pausing scrolling
+        pauseScrollListener();
+    }
+});
+
+document.querySelector(".landing > .fa-chevron-down").addEventListener("click",() => {
+    if (counterScroller === 0){
+        counterScroller = 1;
+        console.log("to first page", counterScroller);
+        scroll();
+
+        //Pausing scrolling
+        pauseScrollListener();
+    }
+});
